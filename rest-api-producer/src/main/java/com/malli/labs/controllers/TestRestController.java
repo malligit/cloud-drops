b1e09a5c5e8aed851fe8ce82afc5d2d7e6a82ab8
Malli Chandana <mallionnet@gmail.com>
2020-03-08 23:41:54 -0500

    React-RestAPI-Example

--- rest-api-producer/src/main/java/com/malli/labs/controllers/TestRestController.java
+++ rest-api-producer/src/main/java/com/malli/labs/controllers/TestRestController.java
@@ -4,6 +4,7 @@ import com.malli.labs.models.DataModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -12,10 +13,19 @@ import java.util.List;
 public class TestRestController {
 
     @GetMapping("/get-user")
-    public DataModel getUser() {
-        System.out.println("get-user method------------------------");
+    public List<DataModel> getUser() {
+        System.out.println("get-user API called");
         List<String> skills = Arrays.asList("Java","PCF","Database");
         DataModel user =  new DataModel("ID001","Malli",new Date(),"Chicago","M",40,skills);
-        return user;
+        List users = new ArrayList<DataModel>();
+        users.add(user);
+        users.add(new DataModel("ID005","Malli5",new Date(),"Chicago","M",40,skills));
+
+        return users;
+    }
+
+    @GetMapping("/msg")
+    public String getMsg() {
+        return "Malli";
     }
 }
