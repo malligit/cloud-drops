6849bbfcfdccea18d39f0f488052ae4993495787
Malli Chandana <mallionnet@gmail.com>
2020-03-14 14:26:38 -0500

    Added method to convert image to PDF

--- Java-Utils-gradle/src/main/java/com/malli/nonweb/labs/PDFMerger.java
+++ Java-Utils-gradle/src/main/java/com/malli/nonweb/labs/PDFMerger.java
@@ -1,8 +1,18 @@
 package com.malli.nonweb.labs;
 
 import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.graphics.xobject.PDJpeg;
+import org.apache.pdfbox.pdmodel.graphics.xobject.PDXObjectImage;
 import org.apache.pdfbox.util.PDFMergerUtility;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -20,12 +30,15 @@ public class PDFMerger {
     public static void main(String[] args) {
 
         String folderPath = "C:\\Malli\\temp";
+        covertToPDF();
+        //mergePDFs(folderPath);
 
-        //Instantiating PDFMergerUtility class
+    }
+
+    private static void mergePDFs(String folderPath) {
         PDFMergerUtility merger = new PDFMergerUtility();
-
         //Setting the destination file
-        merger.setDestinationFileName("C:\\Malli\\temp\\merged-"+System.currentTimeMillis()+".pdf");
+        merger.setDestinationFileName("C:\\Malli\\temp\\merged-" + System.currentTimeMillis() + ".pdf");
 
 
         try {
@@ -41,6 +54,29 @@ public class PDFMerger {
             e.printStackTrace();
         }
         System.out.println("Documents merged");
+    }
+
+    private static void covertToPDF() {
+        try {
+            PDDocument document = new PDDocument();
+            InputStream in = new FileInputStream(new File("C:\\Malli\\temp\\H1B-Ext-2020\\Malli\\IMG-2456.JPG"));
+            BufferedImage bimg = ImageIO.read(in);
+            float width = bimg.getWidth();
+            float height = bimg.getHeight();
+            PDPage page = new PDPage(new PDRectangle(width, height));
+            document.addPage(page);
+            PDXObjectImage img = new PDJpeg(document, new FileInputStream(new File("C:\\Malli\\temp\\H1B-Ext-2020\\Malli\\IMG-2456.JPG")));
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+            contentStream.drawImage(img, 0, 0);
+            contentStream.close();
+            in.close();
+
+            document.save("C:\\Malli\\temp\\test1.pdf");
+            document.close();
+        } catch (Exception e) {
+
+        }
+        System.out.println("Converted to PDF");
     }
 
     private static List<InputStream> getFiles(String folderPath) throws IOException {
